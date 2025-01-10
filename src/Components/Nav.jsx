540094318df7e4e51c68171b54c1d19612dddf4f
Sivabalan <sivabalan.st.2002@gmail.com>
2025-01-11 02:13:10 +0530

    border

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,9 @@
 export function Nav() {
     return <div className="" >
        <nav className="top-0 left-0 container lg:max-w-full w-screen bg-red-300 h-20 border-red-300 shadow-md border flex">
-           <p className="text-red-950 ml-[2px] py-5 text-2xl font-bold ">Portfolio</p>
+           <p className="text-red-950 ml-[20px] py-5 text-2xl lg:text-4xl font-bold ">Portfolio</p>
            <button className="text-red-950 ml-[200px] py-3 font-bold lg:hidden">Menu</button>
-           <ul className="lg:flex  justify-center hidden ">
+           <ul className="lg:flex ml-80 py-3 hidden ">
             <li className="text-red-950 font-semibold text-xl p-4">
                 <a href="#home">Home</a>
             </li>
