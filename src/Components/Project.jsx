0a503f135c77e6d646f8998e3a560e6139511e63
Sivabalan <sivabalan.st.2002@gmail.com>
2025-01-13 14:59:21 +0530

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import "./Styles/Project.css";
-import "./Styles/Animation.css"; 
+import "./Styles/Animation.css";
 
 export function Project() {
   useEffect(() => {
@@ -26,23 +26,26 @@ export function Project() {
   return (
     <section className="py-12">
       <h1 className="text-2xl font-semibold text-center mb-8">Projects</h1>
-      <div className="lg:flex flex-wrap justify-center gap-8">
+      <div className="lg:flex flex-wrap justify-center lg:gap-8 ">
         {/* Project 1 */}
         <div
-          className="group relative w-72 h-48 bg-[url('./S.png')] bg-cover bg-center rounded-xl shadow-lg overflow-hidden scroll-item"
+          className="group relative w-72 h-48  portfolio bg-cover rounded-xl shadow-lg overflow-hidden scroll-item mx-16 my-10 lg:my-0 lg:mx-0"
         >
-          {/* Overlay for Hover */}
-          <div className="absolute inset-0 bg-gradient-to-br from-blue-700 via-purple-600 to-red-700 bg-opacity-50 transform translate-y-full group-hover:translate-y-0 transition duration-500"></div>
-          <div className="absolute inset-0  p-4 opacity-0 group-hover:opacity-100 transition duration-500">
-            <h2 className="text-lg font-bold text-white top-o left-0 flex justify-center">Portfolio</h2>
-            <p className="text-white">This is portfolio of mine created with react.js, jsx, Tailwind CSS and javascript. Respensive.</p>
-            <a href="" className="text-sm text-white underline mt-5">View Project</a>
+          {/* Gradient Background */}
+          <div className="absolute inset-0 bg-gradient-to-r from-red-300 via-red-500 to-red-950 opacity-50"></div>
+          {/* Hover Content */}
+          <div className="absolute inset-0 bg-gradient-to-br from-blue-700 via-purple-600 to-red-700 bg-opacity-50 transform translate-y-full group-hover:translate-y-0 transition duration-500">
+            <div className="absolute inset-0  justify-end p-4 opacity-0 group-hover:opacity-100 transition duration-500">
+              <h2 className="text-xl font-bold text-white">Portfolio</h2>
+              <p className="text-white">This is Portfolio website . Fuly created with HTML, CSS, Javascript, Tailwind css and React js. this is fullu responsive and animated.</p>
+              <a href="https://sivabalansportfolio.netlify.app/" className="text-sm text-white underline mt-2">View Project</a>
+            </div>
           </div>
         </div>
 
         {/* Project 2 */}
         <div
-          className="group relative w-72 h-48  project-2 bg-cover rounded-xl shadow-lg overflow-hidden scroll-item"
+          className="group relative w-72 h-48  gd bg-cover rounded-xl shadow-lg overflow-hidden scroll-item mx-16 my-10 lg:my-0 lg:mx-0"
         >
           {/* Gradient Background */}
           <div className="absolute inset-0 bg-gradient-to-r from-blue-400 via-green-500 to-teal-500 opacity-50"></div>
@@ -50,24 +53,28 @@ export function Project() {
           <div className="absolute inset-0 bg-gradient-to-br from-blue-700 via-purple-600 to-red-700 bg-opacity-50 transform translate-y-full group-hover:translate-y-0 transition duration-500">
             <div className="absolute inset-0  justify-end p-4 opacity-0 group-hover:opacity-100 transition duration-500">
               <h2 className="text-xl font-bold text-white">Google Dorking</h2>
-              <p className="text-white"></p>
-              <a href="#" className="text-sm text-white underline mt-2">View Project</a>
+              <p className="text-white text-[13px]">This is a website named Google Dorking. this is useful for Cyber security learners.
+              Fuly created with HTML, CSS, Javascript, Tailwind css and React js. this is fullu responsive and animated.
+              </p>
+              <a href="https://googledorking.netlify.app/" className="text-sm text-white underline mt-2">View Project</a>
             </div>
           </div>
         </div>
 
         {/* Project 3 */}
         <div
-          className="group relative w-72 h-48 bg-white rounded-xl shadow-lg overflow-hidden scroll-item"
+          className="group relative w-72 h-48 intern  rounded-xl shadow-lg overflow-hidden scroll-item mx-16 lg:mx-0 my-10 lg:my-0"
         >
           {/* Gradient Background */}
           <div className="absolute inset-0 bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 opacity-50"></div>
           {/* Hover Effect */}
-          <div className="absolute inset-0 bg-black bg-opacity-50 transform translate-y-full group-hover:translate-y-0 transition duration-500">
-            <div className="absolute inset-0 flex flex-col justify-end p-4 opacity-0 group-hover:opacity-100 transition duration-500">
-              <h2 className="text-xl font-bold text-white">Project 3</h2>
-              <p className="text-white">ReactJS Portfolio</p>
-              <a href="#" className="text-sm text-white underline mt-2">View Project</a>
+          <div className="absolute inset-0 bg-gradient-to-br from-blue-700 via-purple-600 to-red-700 bg-opacity-50 transform translate-y-full group-hover:translate-y-0 transition duration-500">
+            <div className="absolute inset-0 justify-end p-4 opacity-0 group-hover:opacity-100 transition duration-500">
+              <h2 className="text-xl font-bold text-white">Tech Edufy</h2>
+              <p className="text-white">This is a institute website.I was created my home page of the website during my intern
+                and its fully created by HTML, CSS, Tailwind css and Javascript.
+              </p>
+              <a href="https://websitetechedufy.netlify.app/" className="text-sm text-white underline mt-2">View Project</a>
             </div>
           </div>
         </div>
